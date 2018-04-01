4230ca646d55a5edfc7dda8dd339f77797e10f5d
nir.h <nir.h@sygnia.co>
2018-04-01 11:10:03 +0300

    TQDM fix 1

--- tqdm.py
+++ tqdm.py
@@ -8,7 +8,7 @@ class Tqdm(object):
 
     def __init__(self, iterable):
         self.iterable = iterable
-        self.length = cardinality.count(iterable)
+        self.length = len(iterable) if hasattr(iterable, '__len__') else sum(1 for i in iterable)
 
     def __iter__(self):
         return TqdmIterator(iter(self.iterable), self.length)
@@ -17,21 +17,20 @@ class Tqdm(object):
 class TqdmIterator(object):
 
     def __init__(self, iterator, length):
-        self.start_time = time.time()
+        self.start_time = None
         self.length = length
         self.iterator = iterator
         self.iter_index = 0
 
     def __next__(self):
+        if self.start_time is None:
+            self.start_time = time.time()
         print('\r' + self._get_progress_bar(), end='')
         self.iter_index += 1
         return next(self.iterator)
 
-    def _get_percent(self) -> float:
-            return self.iter_index / self.length * 100
-
     def _get_progress_bar(self) -> str:
-        percent = self._get_percent()
+        percent = self.iter_index / self.length * 100
         passed = int(PROGRESS_BAR_LEN * (percent/100))
 
         hashtags = '#' * passed
