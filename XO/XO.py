9e15b6b8a0c939897fbd5ecd42ed1f51e623c6d0
nir.h <nir.h@sygnia.co>
2018-04-01 14:22:52 +0300

    XO fix 4

--- XO/XO.py
+++ XO/XO.py
@@ -1,8 +1,8 @@
 from typing import List, Union, Tuple
 import random
 
-N = 3
-EMPTY = 'E'
+COLUMNS_NUMBER = 3
+EMPTY_PRINT_SIGN = 'E'
 PLAYERS_SIGNS = ['X', 'O']
 
 
@@ -19,30 +19,31 @@ def get_action(current_player: str, board: List[list]) -> Tuple[int, int]:
     :return: A tuple with the new position coordinates
     """
     error_message = 'Error'
-    row = f'{current_player}, insert line number (0/1/2)\n'
-    column = f'{current_player}, insert column number (0/1/2)\n'
+    column_options = '/'.join([str(i) for i in range(COLUMNS_NUMBER)])
     while error_message:
-        inputs = [input(row), input(column)]
+        inputs = [input(f'{current_player}, insert line number ({column_options})\n'),
+                  input(f'{current_player}, insert column number ({column_options})\n')]
         error_message = is_legal_position(inputs, board)
         if error_message:
             print(error_message)
-    return int(inputs[0]), int(inputs[1])
+    return inputs[0], inputs[1]
 
 
 def is_legal_position(position: List[str], board: List[list]) -> Union[str, None]:
     """
-    Checks if the the coordinates of the game board are legal
+    Checks if the the coordinates of the game board are legal and convert position to list of ints if possible
 
     :param position: The inputs from the user
     :param board: A matrix which represents the game board
     :return: None if the position is legal, or message of the error if the position is not legal
     """
-    for i, user_input in enumerate(position):
-        if not user_input.isdecimal():
+    for i in range(len(position)):
+        if not position[i].isdecimal():
             return f'Error: input number {i} is not a number'
-        if not 0 <= int(user_input) < N:
+        position[i] = int(position[i])
+        if not 0 <= position[i] < COLUMNS_NUMBER:
             return f'Error: input number {i} is not in range'
-    if board[int(position[0])][int(position[1])] is not None:
+    if board[position[0]][position[1]] is not None:
         return 'Error: someone chose this position'
 
 
@@ -50,18 +51,22 @@ def print_board(board: List[list]):
     """
     Prints the current board game as a matrix (N+1)X(N+1)
     The first line and first column are the indexes and the inner cells can be: 'X'\'O'\'E'
+
+    :param board: A matrix which represents the game board
     """
-    print(' \t' + '\t'.join([str(i) for i in range(N)]))
+    print(' \t' + '\t'.join([str(i) for i in range(COLUMNS_NUMBER)]))
     for line_index, line in enumerate(board):
         print_line = []
         for k, cell in enumerate(line):
-            print_line.append(EMPTY if cell is None else PLAYERS_SIGNS[cell])
+            print_line.append(EMPTY_PRINT_SIGN if cell is None else PLAYERS_SIGNS[cell])
         print(f'{line_index}\t' + '\t'.join(print_line))
 
 
 def _are_all_equal(l: list) -> bool:
     """
-    false if there is None in l
+    Checks if all values in the list are equal, except when all values are None.
+
+    :param l: list of the values
     """
     return l[0] is not None and l.count(l[0]) == len(l)
 
@@ -78,14 +83,14 @@ def get_winner(board: List[list]) -> Union[int, None]:
         if _are_all_equal(line):
             return line[0]
     # Check columns.
-    for line in zip(*board):
-        if _are_all_equal(line):
-            return line[0]
+    for column in zip(*board):
+        if _are_all_equal(column):
+            return column[0]
     # Check diagonals.
-    if _are_all_equal([board[i][i] for i in range(N)]):
+    if _are_all_equal([board[i][i] for i in range(COLUMNS_NUMBER)]):
         return board[0][0]
-    if _are_all_equal([board[i][N - 1 - i] for i in range(N)]):
-        return board[0][N - 1]
+    if _are_all_equal([board[i][COLUMNS_NUMBER - 1 - i] for i in range(COLUMNS_NUMBER)]):
+        return board[0][COLUMNS_NUMBER - 1]
 
 
 def run_game():
@@ -93,12 +98,12 @@ def run_game():
 
     # Initialize variables
     turn_index = random.randint(0, 1)
-    board = [[None] * N for i in range(N)]
+    board = [[None] * COLUMNS_NUMBER for i in range(COLUMNS_NUMBER)]
     winner = None
     counter = 0
     # The game starts.
     while winner is None:
-        if counter == N * N:
+        if counter == COLUMNS_NUMBER * COLUMNS_NUMBER:
             print('There is no winner!')
             return
         print_board(board)
