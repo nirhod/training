f66d9dfd7f3c6f4c1f66c90a88a0e572abf44936
nir.h <nir.h@sygnia.co>
2018-04-02 16:00:18 +0300

    SIM_CITY

--- sim_city.py
+++ sim_city.py
@@ -1,38 +1,64 @@
 class City:
+    """
+    Represents a city in Ackland, should help calculate the tax of each city
+    """
 
     def __init__(self):
         self.neighborhoods = {}
         self.base_tax = 1000
 
-    def build_a_neighborhood(self, neighborhood_name):
-        pass
+    def add_new_neighborhood(self, neighborhood_name: str):
+        """
+        Creates a new neighborhood in the city
+        :param neighborhood_name: the name of the neighborhood
+        """
+        self.neighborhoods[neighborhood_name] = Neighborhood()
+        self.base_tax *= 1.1
 
-    def how_much_money(self):
-        pass
+    def add_new_house(self, neighborhood_name: str, family_members: int, size: int):
+        """
+        Creates a new house in a specific neighborhood in the city
+        :param neighborhood_name: the name of the neighborhood
+        :param family_members: Number of the family members
+        :param size: The area of the house
+        """
+        self.neighborhoods[neighborhood_name].add_new_house(family_members, size)
+
+    def remove_neighborhood(self, neighborhood_name: str):
+        """
+        Destroy a neighborhood in the city
+        :param neighborhood_name: the name of the neighborhood
+        """
+        self.neighborhoods.pop(neighborhood_name)
+        self.base_tax *= 1.05
+
+    def add_new_park(self, neighborhood_name: str):
+        """
+        Create a park in a specific neighborhood in the city
+        :param neighborhood_name: the name of the neighborhood
+        """
+        self.neighborhoods[neighborhood_name].parks += 1
+
+    def all_tax(self):
+        """
+        :return: How much tax should the city pay. include base tax, neighborhoods taxes and houses taxes
+        """
+        return self.base_tax + sum([neighborhood.all_tax() for neighborhood in self.neighborhoods.values()])
 
 
 class Neighborhood:
     """
     Represents a neighborhood in a city, should help calculate the tax of each neighborhood
-    Attributes:
-        name: The name of the neighborhood
-        parks: How many parks are in the neighborhood
-        houses: list of houses in the neighborhood
-    Methods:
-        build_a_house: Adds a new house to the neighborhood
-        how_much_money: How much tax should the neighborhood pay, include base tax and houses tax
     """
 
-    def __init__(self, name: str, parks: int=0):
+    def __init__(self, parks: int=0):
         """
-        :param name: The name of the neighborhood
         :param parks: How many parks are in the neighborhood
         """
-        self.name = name
         self.parks = parks
         self.houses = []
 
-    def build_a_house(self, family_members: int, size: int):
+    def add_new_house(self, family_members: int, size: int):
         """
         Adds a new house to the neighborhood
         :param family_members: Number of the family members
@@ -40,21 +66,18 @@ class Neighborhood:
         """
         self.houses.append(House(family_members, size))
 
-    def how_much_money(self) -> int:
+    def all_tax(self) -> int:
         """
-        :return: How much tax should the neighborhood pay, include base tax and houses tax
+        :return: How much tax should the neighborhood pay, include base tax and houses taxes
         """
-        return self.parks * 5 + len(self.houses) * 3 + sum([house.how_much_money() for house in self.houses])
+        return ((self.parks * 5)
+                + (len(self.houses) * 3)
+                + sum([house.all_tax() for house in self.houses]))
 
 
 class House:
     """
     Represents a house in a neighborhood, should help calculate the tax of each house
-    Attributes:
-        family_members: Number of the family members
-        size: The area of the house
-    Methods:
-        how_much_money: how much tax should the house pay
     """
 
     def __init__(self, family_members: int, size: int):
@@ -65,7 +88,7 @@ class House:
         self.family_members = family_members
         self.size = size
 
-    def how_much_money(self) -> int:
+    def all_tax(self) -> int:
         """
         :return: how much tax should the house pay
         """
@@ -73,4 +96,21 @@ class House:
 
 
 if __name__ == '__main__':
-    print('hi')
+    synville = City()
+    print(synville.all_tax())  # 1000
+    synville.add_new_neighborhood('TA')
+    print(synville.all_tax())  # 1100
+    synville.add_new_park('TA')
+    synville.add_new_park('TA')
+    print(synville.all_tax())  # 1110
+    synville.add_new_neighborhood('RG')
+    print(synville.all_tax())
+    synville.add_new_house('TA', 5, 2)
+    print(synville.all_tax())
+    synville.add_new_house('TA', 5, 2)
+    print(synville.all_tax())
+    synville.add_new_house('RG', 2, 5)
+    print(synville.all_tax())  # 1259
+    synville.remove_neighborhood('RG')
+    print(synville.all_tax())  # 1306.5
+
