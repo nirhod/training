0a27c3a5b8afa0ad845e14e301949d8718cf5871
nir.h <nir.h@sygnia.co>
2018-04-03 14:48:14 +0300

    Fixed docstrings and constant for DEFAULT_TAX. Now a neighborhood aware of its name. Added checking if neighborhood exist.

--- sim_city.py
+++ sim_city.py
@@ -1,74 +1,95 @@
 class City:
     """
-    Represents a city in Ackland, should help calculate the tax of each city
+    Represents a city in Ackland, should help calculate the tax of each city.
     """
 
-    def __init__(self):
+    DEFAULT_BASE_TAX = 1000
+
+    def __init__(self, base_tax=DEFAULT_BASE_TAX):
         self.neighborhoods = {}
-        self.base_tax = 1000
+        self.base_tax = base_tax
 
     def add_new_neighborhood(self, neighborhood_name: str):
         """
-        Creates a new neighborhood in the city
-        :param neighborhood_name: the name of the neighborhood
+        Creates a new neighborhood in the city.
+
+        :param neighborhood_name: The name of the neighborhood.
         """
-        self.neighborhoods[neighborhood_name] = Neighborhood()
+        self.neighborhoods[neighborhood_name] = Neighborhood(neighborhood_name)
         self.base_tax *= 1.1
 
     def add_new_house(self, neighborhood_name: str, family_members: int, size: int):
         """
-        Creates a new house in a specific neighborhood in the city
-        :param neighborhood_name: the name of the neighborhood
-        :param family_members: Number of the family members
-        :param size: The area of the house
+        Creates a new house in a specific neighborhood in the city.
+
+        :param neighborhood_name: The name of the neighborhood.
+        :param family_members: Number of the family members.
+        :param size: The area of the house.
         """
+        self._check_neighborhood(neighborhood_name)
         self.neighborhoods[neighborhood_name].add_new_house(family_members, size)
 
     def remove_neighborhood(self, neighborhood_name: str):
         """
-        Destroy a neighborhood in the city
-        :param neighborhood_name: the name of the neighborhood
+        Destroy a neighborhood in the city.
+
+        :param neighborhood_name: The name of the neighborhood.
         """
+        self._check_neighborhood(neighborhood_name)
         self.neighborhoods.pop(neighborhood_name)
         self.base_tax *= 1.05
 
     def add_new_park(self, neighborhood_name: str):
         """
-        Create a park in a specific neighborhood in the city
-        :param neighborhood_name: the name of the neighborhood
+        Create a park in a specific neighborhood in the city.
+
+        :param neighborhood_name: The name of the neighborhood.
         """
+        self._check_neighborhood(neighborhood_name)
         self.neighborhoods[neighborhood_name].parks += 1
+
+    def _check_neighborhood(self, neighborhood_name: str):
+        """
+        Raise an exception if the neighborhood doesn't exist
+
+        :param neighborhood_name: The name of the neighborhood.
+        """
+        if neighborhood_name not in self.neighborhoods:
+            raise KeyError(f'The neighborhood {neighborhood_name} does not exist')
 
     def all_tax(self):
         """
-        :return: How much tax should the city pay. include base tax, neighborhoods taxes and houses taxes
+        :return: How much tax should the city pay. include base tax, neighborhoods taxes and houses taxes.
         """
         return self.base_tax + sum([neighborhood.all_tax() for neighborhood in self.neighborhoods.values()])
 
 
 class Neighborhood:
     """
-    Represents a neighborhood in a city, should help calculate the tax of each neighborhood
+    Represents a neighborhood in a city, should help calculate the tax of each neighborhood.
     """
 
-    def __init__(self, parks: int=0):
+    def __init__(self, neighborhood_name: str, parks: int=0):
         """
-        :param parks: How many parks are in the neighborhood
+        :param neighborhood_name: The name of the neighborhood.
+        :param parks: How many parks are in the neighborhood.
         """
+        self.neighborhood_name = neighborhood_name
         self.parks = parks
         self.houses = []
 
     def add_new_house(self, family_members: int, size: int):
         """
-        Adds a new house to the neighborhood
-        :param family_members: Number of the family members
-        :param size: The area of the house
+        Adds a new house to the neighborhood.
+
+        :param family_members: Number of the family members.
+        :param size: The area of the house.
         """
         self.houses.append(House(family_members, size))
 
     def all_tax(self) -> int:
         """
-        :return: How much tax should the neighborhood pay, include base tax and houses taxes
+        :return: How much tax should the neighborhood pay, include base tax and houses taxes.
         """
         return ((self.parks * 5)
                 + (len(self.houses) * 3)
@@ -77,20 +98,20 @@ class Neighborhood:
 
 class House:
     """
-    Represents a house in a neighborhood, should help calculate the tax of each house
+    Represents a house in a neighborhood, should help calculate the tax of each house.
     """
 
     def __init__(self, family_members: int, size: int):
         """
-        :param family_members: Number of the family members
-        :param size: The area of the house
+        :param family_members: Number of the family members.
+        :param size: The area of the house.
         """
         self.family_members = family_members
         self.size = size
 
     def all_tax(self) -> int:
         """
-        :return: how much tax should the house pay
+        :return: how much tax should the house pay.
         """
         return self.size * self.family_members
 
@@ -114,3 +135,4 @@ if __name__ == '__main__':
     synville.remove_neighborhood('RG')
     print(synville.all_tax())  # 1306.5
 
+    # synville.add_new_park('aaa')
