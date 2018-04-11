7cf12ffca9b40d1b1649366f145709f2d99fb9f1
nir.h <nir.h@sygnia.co>
2018-04-11 11:46:06 +0300

    Add pytest.raises and changed test_save_number and test_load_number.

--- test_pycalculator.py
+++ test_pycalculator.py
@@ -1,13 +1,12 @@
 import pytest
 import pycalculator
-import pickle
 import os
 
 
 @pytest.fixture()
 def temp_file(tmpdir):
     """
-    :return: A path of a temp file that can be overwritten
+    :return: A path of a temp file that can be overwritten.
     """
     return os.path.join(tmpdir, 'temp_file.temp')
 
@@ -23,6 +22,11 @@ def test_div(dividend, divisor, quotient):
     assert pycalculator.div(dividend, divisor) == quotient
 
 
+def test_div_zero_division_exception():
+    with pytest.raises(ZeroDivisionError):
+        pycalculator.div(1, 0)
+
+
 @pytest.mark.parametrize('num1, num2, the_sum',
                          ((5, 10, 15)
                           , (-2, 2, 0)
@@ -33,16 +37,24 @@ def test_sum(num1, num2, the_sum):
     assert pycalculator.sum(num1, num2) == the_sum
 
 
-@pytest.mark.parametrize('num', (5, -1))
-@pytest.mark.xfail
-def test_save_number(num, temp_file):
+def test_sum_add_str_exception():
+    with pytest.raises(TypeError):
+        pycalculator.sum(1, 'a')
+
+
+def test_save_number_is_a_directory_exception():
+    with pytest.raises(IsADirectoryError):
+        pycalculator.save_number(5, '/')
+
+
+@pytest.mark.parametrize('num', (5, -1, 0))
+def test_save_load_number(num, temp_file):
+    """
+    Test both save_number and load_number.
+    Can't divide it to two different tests because I can't know the inner implementation.
+    """
     pycalculator.save_number(num, temp_file)
-    with open(temp_file, 'rb') as num_file:
-        assert pickle.load(num_file) == num
+    assert pycalculator.load_number(temp_file) == num
 
 
-@pytest.mark.parametrize('num', (5, -1))
-def test_load_number(num, temp_file):
-    with open(temp_file, 'wb') as num_file:
-        pickle.dump(num, num_file)
-    assert pycalculator.load_number(temp_file)
+
