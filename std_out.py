8497256d2ce8fe5843b1add2f6b55d435673edaa
nir.h <nir.h@sygnia.co>
2018-04-23 15:10:27 +0300

    no message

--- std_out.py
+++ std_out.py
@@ -3,6 +3,16 @@ import sys
 
 
 class stdout_redirect:
+    """
+    A context manager that redirect the prints of stdout to itself.
+
+    Usage example:
+    with stdout_redirect() as sio:
+        print('Hello world')  # no print to screen.
+        sio.seek(0)
+        string = sio.read()
+    print(string.read())  # 'Hello world' will be printed.
+    """
     def __enter__(self):
         self.old_stdout = sys.stdout
         sys.stdout = io.StringIO()
---
+++ test_std_out.py
@@ -0,0 +1,5 @@
+from std_out import stdout_redirect
+
+
+def test_stdout_redirect():
+    pass
