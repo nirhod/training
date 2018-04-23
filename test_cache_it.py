5eca5cc28291da0fc9e2f6d2347354604e562b6f
nir.h <nir.h@sygnia.co>
2018-04-23 16:15:20 +0300

    Added test_cache_result.

--- test_cache_it.py
+++ test_cache_it.py
@@ -3,7 +3,7 @@ from unittest import mock
 import cache_it
 
 
-def test_cache():
+def test_cache_calls():
     """
     Call twice to a function decorated with cache_it.cache and make sure it was called only once.
     """
@@ -13,3 +13,12 @@ def test_cache():
     assert mock_func.call_count == 1
     cached_mock_func()
     assert mock_func.call_count == 1
+
+
+def test_cache_result():
+    def func(x):
+        return x + 1
+    result = func(5)
+    cached_func = cache_it.cache(func)
+    assert cached_func(5) == result
+    assert cached_func(5) == result  # Checked twice because func won't be called at the second time.
