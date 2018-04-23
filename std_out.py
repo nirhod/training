070c6dc2662b7110d389ac94f608bbd651b18c96
nir.h <nir.h@sygnia.co>
2018-04-23 18:00:21 +0300

    Used contextlib instead of class.

--- std_out.py
+++ std_out.py
@@ -1,23 +1,11 @@
 import io
 import sys
+from contextlib import contextmanager
 
 
-class StdoutRedirect:
-    """
-    A context manager that redirect the prints of stdout to itself.
-
-    Usage example:
-    with stdout_redirect() as sio:
-        print('Hello world')  # No print to screen.
-        sio.seek(0)
-        string = sio.read()
-    print(string.read())  # 'Hello world' will be printed.
-    """
-
-    def __enter__(self):
-        self.old_stdout = sys.stdout
-        sys.stdout = io.StringIO()
-        return sys.stdout
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        sys.stdout = self.old_stdout
+@contextmanager
+def std_out_redirect():
+    old_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    yield sys.stdout
+    sys.stdout = old_stdout
--- test_std_out.py
+++ test_std_out.py
@@ -1,17 +1,17 @@
 import sys
 
-from std_out import StdoutRedirect
+from std_out import std_out_redirect
 
 
 def test_stdout_redirect_restore_stdout():
     old_stdout = sys.stdout
-    with StdoutRedirect():
+    with std_out_redirect():
         pass
     assert old_stdout == sys.stdout
 
 
 def test_stdout_redirect_out():
-    with StdoutRedirect() as sio:
+    with std_out_redirect() as sio:
         print('hello world')
         sio.seek(0)
         stdout_redirected_output = sio.read()
@@ -19,7 +19,7 @@ def test_stdout_redirect_out():
 
 
 def test_stdout_redirect_no_print(capfd):
-    with StdoutRedirect():
+    with std_out_redirect():
         print('hello world')
         print_out, err = capfd.readouterr()
     assert not print_out
