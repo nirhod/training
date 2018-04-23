d951b8caa50c479e00c13eca8584ee1b8b2748e0
nir.h <nir.h@sygnia.co>
2018-04-23 18:05:17 +0300

    Added docstring.

--- std_out.py
+++ std_out.py
@@ -5,6 +5,9 @@ from contextlib import contextmanager
 
 @contextmanager
 def std_out_redirect():
+    """
+    A context manager that redirect the prints of stdout to itself.
+    """
     old_stdout = sys.stdout
     sys.stdout = io.StringIO()
     yield sys.stdout
