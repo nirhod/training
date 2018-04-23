8829f9e64b2d1be2b251f74ea9424c175ed0e512
nir.h <nir.h@sygnia.co>
2018-04-23 17:45:19 +0300

    Removed fixture.

--- test_std_out.py
+++ test_std_out.py
@@ -1,6 +1,4 @@
-import pytest
 import sys
-from collections import namedtuple
 
 from std_out import stdout_redirect
 
@@ -12,20 +10,16 @@ def test_stdout_redirect_restore_stdout():
     assert old_stdout == sys.stdout
 
 
-def test_stdout_redirect_out(use_stdout_redirect):
-    assert use_stdout_redirect.stdout_redirect_out == 'hello world\n'
-
-
-def test_stdout_redirect_no_print(use_stdout_redirect):
-    assert not use_stdout_redirect.out
-
-
-@pytest.fixture
-def use_stdout_redirect(capfd):
+def test_stdout_redirect_out():
     with stdout_redirect() as sio:
         print('hello world')
-        out, err = capfd.readouterr()
         sio.seek(0)
-        stdout_redirect_out = sio.read()
-    Return = namedtuple('Return', ('out', 'stdout_redirect_out'))
-    return Return(out, stdout_redirect_out)
+        stdout_redirected_output = sio.read()
+    assert stdout_redirected_output == 'hello world\n'
+
+
+def test_stdout_redirect_no_print(capfd):
+    with stdout_redirect():
+        print('hello world')
+        print_out, err = capfd.readouterr()
+    assert not print_out
