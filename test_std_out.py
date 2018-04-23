9a016f76040e0959f92308e14b39eab79d286b77
nir.h <nir.h@sygnia.co>
2018-04-23 15:17:47 +0300

    no message

--- test_std_out.py
+++ test_std_out.py
@@ -1,5 +1,10 @@
+import pytest
+
 from std_out import stdout_redirect
 
 
-def test_stdout_redirect():
-    pass
+def test_stdout_redirect(capfd):
+    print('hello world')
+    # out, err = capfd.readouterr()
+    x = 1
+    assert False
