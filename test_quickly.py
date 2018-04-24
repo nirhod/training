d4a24678fc6d450105a62b19083be527418a8080
nir.h <nir.h@sygnia.co>
2018-04-24 11:37:56 +0300

    Fixed is_timer_tenth_second and added support for high load machine.

--- test_quickly.py
+++ test_quickly.py
@@ -1,6 +1,8 @@
-from quickly import timer
+import datetime
 from time import sleep
 import pytest
+
+from quickly import timer
 
 
 EPSILON = 0.01
@@ -8,27 +10,35 @@ TENTH_SECOND = 0.1
 
 
 def test_timer_once(timer_gen):
-    assert is_timer_tenth_second(timer_gen)
+    start = datetime.datetime.now()
+    timer_avg_time = next(timer_gen)
+    real_avg_time = (datetime.datetime.now() - start).total_seconds()
+    assert is_timer_time_eq_real_time(timer_avg_time, real_avg_time)
 
 
 def test_timer_many_times(timer_gen):
+    start = datetime.datetime.now()
     for i in range(14):
         next(timer_gen)
-    assert is_timer_tenth_second(timer_gen)
+    timer_avg_time = next(timer_gen)
+    real_avg_time = (datetime.datetime.now() - start).total_seconds() / 15
+    assert is_timer_time_eq_real_time(timer_avg_time, real_avg_time)
 
 
 def test_timer_send_many_times(timer_gen):
     next(timer_gen)
-    timer_gen.send(14)
-    assert is_timer_tenth_second(timer_gen)
+    start = datetime.datetime.now()
+    timer_avg_time = timer_gen.send(15)
+    real_avg_time = (datetime.datetime.now() - start).total_seconds() / 15
+    assert is_timer_time_eq_real_time(timer_avg_time, real_avg_time)
 
 
 def sleep_tenth_second():
     sleep(TENTH_SECOND)
 
 
-def is_timer_tenth_second(used_timer_gen):
-    return TENTH_SECOND - EPSILON < next(used_timer_gen) < TENTH_SECOND + EPSILON
+def is_timer_time_eq_real_time(timer_avg_time, real_avg_time):
+    return real_avg_time - EPSILON < timer_avg_time < real_avg_time + EPSILON
 
 
 @pytest.fixture
